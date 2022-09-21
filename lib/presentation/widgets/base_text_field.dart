4d2aad6ed807e273e3c6c6fe451f9471405baf9c
RUzai <RUzairov@gmail.com>
2022-09-21 23:34:34 +0300

    fixes

--- lib/presentation/widgets/base_text_field.dart
+++ lib/presentation/widgets/base_text_field.dart
@@ -6,7 +6,7 @@ class BaseTextField extends StatelessWidget {
   final EdgeInsets margin;
   final TextEditingController? controller;
   final String? placeholder;
-  final Widget icon;
+  final Widget? icon;
   final VoidCallback? onPressed;
 
   const BaseTextField({
@@ -14,27 +14,27 @@ class BaseTextField extends StatelessWidget {
     this.margin = EdgeInsets.zero,
     this.controller,
     this.placeholder,
-    this.icon = const SizedBox(),
+    this.icon,
     this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: margin,
-      child: Container(
-        decoration: const BoxDecoration(boxShadow: Styles.shadows),
-        child: TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            hintText: placeholder ?? '',
-            suffixIcon: IconButton(icon: icon, onPressed: onPressed),
-            border: const OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
+    return Container(
+      margin: margin,
+      decoration: const BoxDecoration(boxShadow: Styles.shadows),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: placeholder ?? '',
+          suffixIcon: icon == null
+              ? null
+              : IconButton(icon: icon!, onPressed: onPressed),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
         ),
       ),
