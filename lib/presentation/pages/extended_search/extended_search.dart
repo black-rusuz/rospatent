d81cd4502c229fd94277e1f5f2e29d0af183c857
gsusha <x.dyachenko@gmail.com>
2022-09-22 05:00:18 +0300

    part of extended search fix

--- lib/presentation/pages/extended_search/extended_search.dart
+++ lib/presentation/pages/extended_search/extended_search.dart
@@ -32,7 +32,7 @@ class _ExtendedSearchState extends State<ExtendedSearch> {
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
         ),
       ),
-      body: Column(
+      body: ListView(
         children: [
           const SearchField(),
           BaseTextField(
